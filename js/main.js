294897f5947118a050fafe68d322e9b35bf2bb06
nacim harfouche <nacim_4@hotmail.fr>
2020-03-22 01:02:42 +0100

    add some improvement

--- js/main.js
+++ js/main.js
@@ -1,19 +1,61 @@
+/**
+ * Variable
+ */
 let header = $("header");
-let divHeader = $("header>div");
 
-//au lancement de la page
+ /**
+  * Function
+  */
+
+
+/**
+ * at the launch of the page
+ */
 $(() => {
 	divHeader.hide().show(1300);
-	console.log(document.location.href)
+	console.log(document.location.href);
 });
 
-console.log(document.location.pathname);
+/**
+ * button go to top
+ */
+// create i
+let iElt = document.createElement("i");
+iElt.className = "fas fa-chevron-up";
+// create a button
+let buttonElt = document.createElement("button");
+buttonElt.id = "myBtnToTop";
+// buttonElt.textContent = "Top";
 
-// let footer = document.querySelector("footer");
-// footer.textContent = `
-// <p class="text-center">© 2019, All Rights Reserved - 
-// <a href="https://github.com/NacimHarfouche" target="_blank" rel="noopener noreferrer">Nacim Harfouche</a>
-// <a href="https://twitter.com/ArtesEveni" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
-// <a href="https://www.linkedin.com/in/nacim-harfouche-912b14125/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
-// </p>
-// `;
+// append the element in the DOM
+buttonElt.appendChild(iElt);
+document.body.appendChild(buttonElt);
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = () => {
+	buttonElt.style.display = document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? "block" : "none";
+};
+
+// When the user clicks on the button, scroll to the top of the document
+buttonElt.addEventListener("click", () => {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
+// click on nav link
+$('header nav div div a').click(() => {
+	// screen width tab or mobile
+	if ($(window).width() <= 991) {
+		$('header button').click();
+	}
+	// empty main
+	$('main').empty();
+
+	// for the EN and FR version
+	if (/\/en\//i.test(document.location.pathname)) {
+		console.log("en : ", document.location.pathname);
+	} else {
+		console.log("fr : ", document.location.pathname)
+	}
+	$("section").addClass("rounded");
+});
